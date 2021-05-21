20f651aab8abe9a4c7167d9e54041145bc23025e
unknown <karan.jagtap195@gmail.com>
2021-05-21 11:48:10 +0530

    Add Original and Summary word counts

--- summaryzer_gui.py
+++ summaryzer_gui.py
@@ -1,109 +1,27 @@
 # Core Packages
-# Import Heapq for Finding the Top N Sentences
-# from nltk_summarization import nltk_summarizer
-# from spacy_summarization import text_summarizer
-import heapq
-from gensim.summarization import summarize
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
-import nltk
-from heapq import nlargest
-from string import punctuation
-from spacy.lang.en.stop_words import STOP_WORDS
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+# Core Packages
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter.scrolledtext import *
 import tkinter.filedialog
-# Pkgs for Normalizing Text
-import spacy
-nlp = spacy.load('en')
 
 # NLP Pkgs
-# text_summarizer
-
-
-def text_summarizer(raw_docx):
-    raw_text = raw_docx
-    docx = nlp(raw_text)
-    stopwords = list(STOP_WORDS)
-    # Build Word Frequency # word.text is tokenization in spacy
-    word_frequencies = {}
-    for word in docx:
-        if word.text not in stopwords:
-            if word.text not in word_frequencies.keys():
-                word_frequencies[word.text] = 1
-            else:
-                word_frequencies[word.text] += 1
-
-    maximum_frequncy = max(word_frequencies.values())
-
-    for word in word_frequencies.keys():
-        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
-    # Sentence Tokens
-    sentence_list = [sentence for sentence in docx.sents]
-
-    # Sentence Scores
-    sentence_scores = {}
-    for sent in sentence_list:
-        for word in sent:
-            if word.text.lower() in word_frequencies.keys():
-                if len(sent.text.split(' ')) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word.text.lower()]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word.text.lower()]
-
-    summarized_sentences = nlargest(
-        7, sentence_scores, key=sentence_scores.get)
-    final_sentences = [w.text for w in summarized_sentences]
-    summary = ' '.join(final_sentences)
-    return summary
-
-# nltk_summarizer
-
-
-def nltk_summarizer(raw_text):
-    stopWords = set(stopwords.words("english"))
-    word_frequencies = {}
-    for word in nltk.word_tokenize(raw_text):
-        if word not in stopWords:
-            if word not in word_frequencies.keys():
-                word_frequencies[word] = 1
-            else:
-                word_frequencies[word] += 1
-
-    maximum_frequncy = max(word_frequencies.values())
-
-    for word in word_frequencies.keys():
-        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
-
-    sentence_list = nltk.sent_tokenize(raw_text)
-    sentence_scores = {}
-    for sent in sentence_list:
-        for word in nltk.word_tokenize(sent.lower()):
-            if word in word_frequencies.keys():
-                if len(sent.split(' ')) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word]
-
-    summary_sentences = heapq.nlargest(
-        7, sentence_scores, key=sentence_scores.get)
-
-    summary = ' '.join(summary_sentences)
-    return summary
-
+from spacy_summarization import text_summarizer
+from gensim.summarization import summarize
+from nltk_summarization import nltk_summarizer
 
 # Web Scraping Pkg
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 # Structure and Layout
 window = Tk()
-window.title(" Text Summarizer GUI")
-window.geometry("1000x800")
+window.title(" Text Summarizer ")
+window.geometry("900x800")
 window.config(background='black')
 
 style = ttk.Style(window)
@@ -120,29 +38,43 @@ tab4 = ttk.Frame(tab_control)
 tab5 = ttk.Frame(tab_control)
 
 # ADD TABS TO NOTEBOOK
-tab_control.add(tab1, text=f'{"Home":^20s}')
-tab_control.add(tab2, text=f'{"File":^20s}')
-tab_control.add(tab3, text=f'{"URL":^20s}')
-tab_control.add(tab4, text=f'{"Comparer ":^20s}')
-tab_control.add(tab5, text=f'{"About ":^20s}')
+tab_control.add(tab1, text=f'{"Home":^40s}')
+tab_control.add(tab2, text=f'{"File":^40s}')
+tab_control.add(tab3, text=f'{"URL":^40s}')
+tab_control.add(tab4, text=f'{"Comparer ":^40s}')
+tab_control.add(tab5, text=f'{"About ":^40s}')
 
 
-label1 = Label(tab1, text='Summaryzer', padx=5, pady=5)
+label1 = Label(tab1, font="h1", text='Text Summarizer', padx=5, pady=5)
 label1.grid(column=0, row=0)
 
-label2 = Label(tab2, text='File Processing', padx=5, pady=5)
+label2 = Label(tab2, font="h1", text='File Processing', padx=5, pady=5)
 label2.grid(column=0, row=0)
 
-label3 = Label(tab3, text='URL', padx=5, pady=5)
+label3 = Label(tab3, font="h1", text='URL', padx=5, pady=5)
 label3.grid(column=0, row=0)
 
-label3 = Label(tab4, text='Compare Summarizers', padx=5, pady=5)
+label3 = Label(tab4, font="h1", text='Compare Summarizers', padx=5, pady=5)
 label3.grid(column=0, row=0)
 
-label4 = Label(tab5, text='About', padx=5, pady=5)
+label4 = Label(tab5, font="h1", text='About', padx=5, pady=5)
 label4.grid(column=0, row=0)
 
 tab_control.pack(expand=1, fill='both')
+
+# Word Count of Original Text and Summary
+original_word_count = 0
+summary_word_count = 0
+
+
+# def original_word_count(value):
+#     original_word_count = value
+#     return original_word_count
+
+
+# def summary_word_count(value):
+#     summary_word_count = value
+#     return summary_word_count
 
 # Functions
 
@@ -153,9 +85,13 @@ def get_summary():
     print(final_text)
     result = '\nSummary:{}'.format(final_text)
     tab1_display.insert(tk.END, result)
-
+    global summary_word_count
+    # summary_word_count = len(result)
+    # print(summary_word_count)
 
 # Clear entry widget
+
+
 def clear_text():
     entry.delete('1.0', END)
 
@@ -196,6 +132,8 @@ def clear_compare_display_result():
 
 # Functions for TAB 2 FILE PROCESSER
 # Open File to Read and Process
+
+
 def openfiles():
     file1 = tkinter.filedialog.askopenfilename(
         filetypes=(("Text Files", ".txt"), ("All files", "*")))
@@ -215,7 +153,7 @@ def get_file_summary():
 def get_text():
     raw_text = str(url_entry.get())
     page = urlopen(raw_text)
-    soup = BeautifulSoup(page)
+    soup = BeautifulSoup(page, features="lxml")
     fetched_text = ' '.join(map(lambda p: p.text, soup.find_all('p')))
     url_display.insert(tk.END, fetched_text)
 
@@ -228,7 +166,6 @@ def get_url_summary():
 
 
 # COMPARER FUNCTIONS
-
 def use_spacy():
     raw_text = str(entry1.get('1.0', tk.END))
     final_text = text_summarizer(raw_text)
@@ -262,11 +199,24 @@ def use_sumy():
 
 
 # MAIN NLP TAB
-l1 = Label(tab1, text="Enter Text To Summarize")
+def main_tab_entry_update(event):
+    entry_var.set("Original Word Count: " +
+                  str(len(entry.get("1.0", 'end-1c'))))
+
+
+entry_var = StringVar()
+
+og_word_count_main_tab = Label(tab1, textvariable=entry_var)
+og_word_count_main_tab.grid(row=1, column=1)
+
+l1 = Label(tab1, text="Enter Text To Summarize: ")
 l1.grid(row=1, column=0)
 
-entry = Text(tab1, height=10)
+entry = Text(tab1, wrap=WORD, height=18)
 entry.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+entry.bind("<KeyRelease>", main_tab_entry_update)
+
 
 # BUTTONS
 button1 = Button(tab1, text="Reset", command=clear_text,
@@ -274,7 +224,7 @@ button1 = Button(tab1, text="Reset", command=clear_text,
 button1.grid(row=4, column=0, padx=10, pady=10)
 
 button2 = Button(tab1, text="Summarize", command=get_summary,
-                 width=12, bg='#ced', fg='#fff')
+                 width=12, bg='blue', fg='#fff')
 button2.grid(row=4, column=1, padx=10, pady=10)
 
 button3 = Button(tab1, text="Clear Result",
@@ -285,16 +235,44 @@ button4 = Button(tab1, text="Main Points", width=12, bg='#03A9F4', fg='#fff')
 button4.grid(row=5, column=1, padx=10, pady=10)
 
 # Display Screen For Result
-tab1_display = Text(tab1)
+
+
+def main_tab_result_update(event):
+    tab1_display_var.set("Summary Word Count: " +
+                         str(len(tab1_display.get("1.0", 'end-1c'))))
+
+
+tab1_display_var = StringVar()
+
+summ_word_count_main_tab = Label(tab1, textvariable=tab1_display_var)
+summ_word_count_main_tab.grid(row=6, column=1)
+
+tab1_display = Text(tab1, wrap=WORD, height=18)
 tab1_display.grid(row=7, column=0, columnspan=3, padx=5, pady=5)
 
+tab1_display.bind("<KeyRelease>", main_tab_result_update)
 
 # FILE PROCESSING TAB
-l1 = Label(tab2, text="Open File To Summarize")
-l1.grid(row=1, column=1)
 
-displayed_file = ScrolledText(tab2, height=7)  # Initial was Text(tab2)
+
+def file_tab_entry_update(event):
+    displayed_file_var.set("Original Word Count: " +
+                           str(len(displayed_file.get("1.0", 'end-1c'))))
+
+
+displayed_file_var = StringVar()
+
+l1 = Label(tab2, text="Open File To Summarize")
+l1.grid(row=1, column=0)
+
+og_word_count_file_tab = Label(tab2, textvariable=displayed_file_var)
+og_word_count_file_tab.grid(row=1, column=1)
+
+displayed_file = ScrolledText(
+    tab2, wrap=WORD, height=17)  # Initial was Text(tab2)
 displayed_file.grid(row=2, column=0, columnspan=3, padx=5, pady=3)
+
+displayed_file.bind("<KeyRelease>", file_tab_entry_update)
 
 # BUTTONS FOR SECOND TAB/FILE READING TAB
 b0 = Button(tab2, text="Open File", width=12, command=openfiles, bg='#c5cae9')
@@ -315,13 +293,26 @@ b4 = Button(tab2, text="Close", width=12, command=window.destroy)
 b4.grid(row=5, column=2, padx=10, pady=10)
 
 # Display Screen
-# tab2_display_text = Text(tab2)
-tab2_display_text = ScrolledText(tab2, height=10)
+
+
+def file_tab_result_update(event):
+    tab2_display_text_var.set("Summary Word Count: " +
+                              str(len(tab2_display_text.get("1.0", 'end-1c'))))
+
+
+tab2_display_text_var = StringVar()
+
+summ_word_count_file_tab = Label(tab2, textvariable=tab2_display_text_var)
+summ_word_count_file_tab.grid(row=6, column=1)
+
+tab2_display_text = Text(tab2)
+tab2_display_text = ScrolledText(tab2, wrap=WORD, height=17)
 tab2_display_text.grid(row=7, column=0, columnspan=3, padx=5, pady=5)
 
 # Allows you to edit
 tab2_display_text.config(state=NORMAL)
 
+tab2_display_text.bind("<KeyRelease>", file_tab_result_update)
 
 # URL TAB
 l1 = Label(tab3, text="Enter URL To Summarize")
@@ -348,22 +339,61 @@ button4 = Button(tab3, text="Summarize", command=get_url_summary,
                  width=12, bg='#03A9F4', fg='#fff')
 button4.grid(row=5, column=1, padx=10, pady=10)
 
-# Display Screen For Result
-url_display = ScrolledText(tab3, height=10)
+# Display Screen For URL Result
+
+
+def url_tab_entry_update(event):
+    url_display_var.set("Original Word Count: " +
+                        str(len(url_display.get("1.0", 'end-1c'))))
+
+
+url_display_var = StringVar()
+
+og_word_count_url_tab = Label(tab3, textvariable=url_display_var)
+og_word_count_url_tab.grid(row=6, column=1)
+
+url_display = ScrolledText(tab3, wrap=WORD, height=18)
 url_display.grid(row=7, column=0, columnspan=3, padx=5, pady=5)
 
+url_display.bind("<KeyRelease>", url_tab_entry_update)
 
-tab3_display_text = ScrolledText(tab3, height=10)
+# Display Screen For URL Summary Result
+
+
+def url_tab_summary_update(event):
+    tab3_display_text_var.set("Summary Word Count: " +
+                              str(len(tab3_display_text.get("1.0", 'end-1c'))))
+
+
+tab3_display_text_var = StringVar()
+
+summ_word_count_url_tab = Label(tab3, textvariable=tab3_display_text_var)
+summ_word_count_url_tab.grid(row=8, column=1)
+
+tab3_display_text = ScrolledText(tab3, height=18)
 tab3_display_text.grid(row=10, column=0, columnspan=3, padx=5, pady=5)
+
+tab3_display_text.bind("<KeyRelease>", url_tab_summary_update)
 
 
 # COMPARER TAB
+def comparer_tab_entry_update(event):
+    entry1_var.set("Original Word Count: " +
+                   str(len(entry1.get("1.0", 'end-1c'))))
+
+
+entry1_var = StringVar()
+
+og_word_count_comparer_tab = Label(tab4, textvariable=entry1_var)
+og_word_count_comparer_tab.grid(row=1, column=2)
+
 l1 = Label(tab4, text="Enter Text To Summarize")
 l1.grid(row=1, column=0)
 
-entry1 = ScrolledText(tab4, height=10)
+entry1 = ScrolledText(tab4, wrap=WORD, height=18)
 entry1.grid(row=2, column=0, columnspan=3, padx=5, pady=3)
 
+entry1.bind("<KeyRelease>", comparer_tab_entry_update)
 # BUTTONS
 button1 = Button(tab4, text="Reset", command=clear_compare_text,
                  width=12, bg='#03A9F4', fg='#fff')
@@ -397,13 +427,25 @@ choice_button.grid(row=6, column=1)
 
 
 # Display Screen For Result
-tab4_display = ScrolledText(tab4, height=15)
+def comparer_tab_summary_update(event):
+    tab4_display_var.set("Summary Word Count: " +
+                         str(len(tab4_display.get("1.0", 'end-1c'))))
+
+
+tab4_display_var = StringVar()
+
+summ_word_count_summary_tab = Label(
+    tab4, textvariable=tab4_display_var)
+
+summ_word_count_summary_tab.grid(row=6, column=2)
+
+tab4_display = ScrolledText(tab4, wrap=WORD, height=18)
 tab4_display.grid(row=7, column=0, columnspan=3, padx=5, pady=5)
 
-
+tab4_display.bind("<KeyRelease>", comparer_tab_summary_update)
 # About TAB
 about_label = Label(
     tab5, text="Text Summarizer with Deep Learning \n A project by Karan, Vikrant & Anurag", pady=5, padx=5)
-about_label.grid(column=0, row=1)
+about_label.grid(column=0, row=2)
 
 window.mainloop()
